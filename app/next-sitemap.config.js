627c7de0f082303f3c6fd1bbca553b50ac50829f
Farhan Muhammad <47453676+farhan200024@users.noreply.github.com>
2025-11-03 04:27:16 +0000

    Add sitemap and robots.txt

---
+++ app/next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ai-writer-tools-clean-4t5p.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 7000,
+};
